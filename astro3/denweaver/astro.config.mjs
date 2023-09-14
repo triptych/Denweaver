7be5cea598d08a4909f10354acbf4d602d338869
Andrew Wooldridge <triptych@gmail.com>
2023-09-14 12:36:59 -0700

    adding svelte support

--- astro3/denweaver/astro.config.mjs
+++ astro3/denweaver/astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+  output: "server",
+  adapter: netlify()
+});
