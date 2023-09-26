f3b6a1fa7af40ee21b3f014b8188c690b71269c7
Andrew Wooldridge <triptych@gmail.com>
2023-09-26 12:26:38 -0700

    adding tiles and game proto changes

--- astro3/denweaver/public/js/game/game_scene.js
+++ astro3/denweaver/public/js/game/game_scene.js
@@ -3,9 +3,13 @@ export const loadGameScene = () => {
   console.log("loadGameScene called!");
 
   // add a black background to the scene
-  const background = add([
-    rect(width(), height()),
-    color(0, 0, 0),
-    z(-2),
-  ]);
+  const background = add([rect(width(), height()), color(0, 0, 0), z(-2)]);
+
+  loadSprite("apple", "https://kaboomjs.com/sprites/apple.png");
+  // loadSprite("castle", "../../public/img/logo1.png");
+  loadSprite("floor", "../../img/tiles/floor_sand_stone_0.png");
+
+  const floor = add([
+    sprite("floor")
+  ])
 }
