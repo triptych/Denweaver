7237102556e64d2bc4f1ee8454d994df03dcd66e
Andrew Wooldridge <triptych@gmail.com>
2023-09-26 18:40:27 -0700

    adding level + player movement

--- astro3/denweaver/public/js/game/game_scene.js
+++ astro3/denweaver/public/js/game/game_scene.js
@@ -8,8 +8,86 @@ export const loadGameScene = () => {
   loadSprite("apple", "https://kaboomjs.com/sprites/apple.png");
   // loadSprite("castle", "../../public/img/logo1.png");
   loadSprite("floor", "../../img/tiles/floor_sand_stone_0.png");
+  loadSprite("wall", "../../img/tiles/brick_brown_0.png");
+  loadSprite("char", "../../img/char/halfling_new.png");
 
-  const floor = add([
-    sprite("floor")
-  ])
+
+  scene("main", (lvlIdx) => {
+    const SPEED = 320;
+
+    const levels = [
+      [
+        "============",
+        "=++++++++++=",
+        "=++++++++++=",
+        "=++++++++++=",
+        "=++++++++++=",
+        "=++++++++++=",
+        "=++++++++++=",
+        "=++++++++++=",
+        "=++++++++++=",
+        "=++++++++++=",
+        "=++++++++++=",
+        "============",
+      ],
+      // add more levels later to switch to
+    ];
+
+
+    const level = addLevel(levels[lvlIdx], {
+      tileWidth: 32,
+      tileHeight: 32,
+      pos: vec2(32, 32),
+      tiles: {
+        "=": () => [
+          sprite("wall"),
+          area(),
+          body({ isStatic: true }),
+          anchor("center"),
+        ],
+        "+": () => [
+          sprite("floor"),
+          area(),
+          anchor('center')
+        ],
+        "@": () => [
+          sprite("char"),
+          area(),
+          body(),
+          anchor("center"),
+          "player"],
+      },
+    });
+
+    // const player = level.get("player")[0];
+
+    const player = level.spawn([
+      sprite('char'),
+      area(),
+      body(),
+      anchor("center"),
+      "player"
+    ], 2,2)
+    const dirs = {
+      left: LEFT,
+      right: RIGHT,
+      up: UP,
+      down: DOWN,
+    };
+
+
+    for (const dir in dirs) {
+      onKeyPress(dir, () => {
+        //  dialog.dismiss();
+      });
+      onKeyDown(dir, () => {
+        player.move(dirs[dir].scale(SPEED));
+
+      });
+    }
+
+
+  });
+
+    go("main", 0);
 }
